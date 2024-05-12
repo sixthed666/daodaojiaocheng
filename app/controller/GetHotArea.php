e589b8b190b8a28c71f4197de1c880aabac84734
sixthed666 <103234445+sixthed666@users.noreply.github.com>
2024-05-12 21:57:26 +0800

    Add files via upload

---
+++ app/controller/GetHotArea.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\controller;
+
+use think\Request;
+use think\Db;
+
+class GetHotArea
+{
+    public function index(Db $db)
+    {
+        $items = $db->table('item')->select();
+
+        if ($items) {
+            $data = [];
+            foreach ($items as $item) {
+                $data[] = [
+                    'id' => $item['i_id'],
+                    'title' => $item['i_title'],
+                    'imageUrl' => $item['i_img']
+                ];
+            }
+
+            $returnValue = [
+                "code" => 200,
+                "value" => $data
+            ];
+
+            return json($returnValue);
+        } else {
+            $returnValue = [
+                "code" => 100,
+                "value" => "Error fetching data from the database"
+            ];
+
+            return json($returnValue);
+        }
+    }
+
+    public function search_with_iid(Request $request, Db $db)
+    {
+        $iid = $request->get('iid', 0);
+
+        $hotareas_String = $db->table('hotareas')->select();
+
+        $data = json_decode($hotareas_String, true);
+        if ($data !== null) {
+            $nestedJson = json_decode($data[$iid]['json'], true);
+
+            $returnValue = [
+                "code" => 200,
+                "value" => array(
+                    "url" =>$nestedJson["url"],
+                    "top" =>$nestedJson["top"],
+                    "left" =>$nestedJson["left"],
+                    "width" =>$nestedJson["width"],
+                    "height" =>$nestedJson["height"]
+                    )
+            ];
+            return json($returnValue);
+        } else {
+            $returnValue = [
+                "code" => 100,
+                "value" => "Error decoding JSON data"
+            ];
+            return json($returnValue);
+        }
+    }
+}
